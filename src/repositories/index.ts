33b71eccb11059e5a32b907c6cd56bacf5b489b4
adrianolopees <adrianodeev1@gmail.com>
2026-03-17 11:46:02 -0300

    refactor: Explicitly export functions from repositories for improved clarity and maintainability

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,3 @@
-export * from './customerRepository';
-export * from './storeSettingsRepository';
+export { getAllCustomers, createCustomer, updateCustomer, deleteCustomerById, findArchivedCustomers, findCompletedCustomers } from './customerRepository';
+export { onStoreSettingsChange, getStoreSettings, createStore, saveStore, deleteStore, createSalesperson, deleteSalesperson } from './storeSettingsRepository';
+export { getUserById, createUser, getUserWorkspace } from './userRepository';
